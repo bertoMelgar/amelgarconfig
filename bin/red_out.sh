ee715bdce8e12a7f01096924523ebde9b3a36b0c
pocoO <berto@melgargarcia.net>
2008-09-14 18:26:31 +0100

    ficheros necesarios para estado.sh - red

---
+++ bin/red_out.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+IN=  #variable global
+oIN=0
+seg=1
+kil=1000
+kps=$kil*$seg
+
+#funcion que devuelve bytes de entrada
+getIN() {
+  fIN=`grep eth1 /proc/net/dev | sed 's/:/\ /' | awk '{print $10}'`
+  IN=$fIN
+  return 0
+}
+
+getIN
+oIN=$IN
+
+sleep 1
+
+getIN
+nIN=$IN
+
+cal=$[(`echo $oIN-$nIN | bc`)]
+#echo $cal
+
+cal3=$[(`echo "-($cal)/$kps" | bc`)]
+echo $cal3
